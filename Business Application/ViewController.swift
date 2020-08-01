4e1c970a21652e356ddb57a307df8aed7b597de6
Benjamin Inemugha <bennyblasco@gmail.com>
2020-08-01 22:30:03 +0100

    Initial Commit

---
+++ Business Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Business Application
+//
+//  Created by Benjamin Inemugha on 01/08/2020.
+//  Copyright © 2020 Techeloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
