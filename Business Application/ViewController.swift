9881be650b8d562b7997367c4f5c1605380211e6
Benjamin Inemugha <bennyblasco@gmail.com>
2020-08-11 23:04:35 +0100

    Change in the main storyboard

--- Business Application/ViewController.swift
+++ Business Application/ViewController.swift
@@ -15,29 +15,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var aboutUsBtn: UIButton!
     @IBOutlet weak var contactUsBtn: UIButton!
     @IBOutlet weak var socialLinksBtn: UIButton!
+    @IBOutlet weak var techelopersImg: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        servicesBtn.layer.cornerRadius = 5
-        portfolioBtn.layer.cornerRadius = 5
-        aboutUsBtn.layer.cornerRadius = 5
-        contactUsBtn.layer.cornerRadius = 5
-        socialLinksBtn.layer.cornerRadius = 5
+        servicesBtn.layer.cornerRadius = 10
+        portfolioBtn.layer.cornerRadius = 10
+        aboutUsBtn.layer.cornerRadius = 10
+        contactUsBtn.layer.cornerRadius = 10
+        socialLinksBtn.layer.cornerRadius = 10
+        techelopersImg.layer.cornerRadius = 20
     }
 
     @IBAction func serviceBtnWasPressed(_ sender: Any) {
         print("Service Pressed")
+        self.tabBarController?.selectedIndex = 2
     }
     
     @IBAction func portfolioBtnWasPressed(_ sender: Any) {
         print("Portfolio Pressed")
+        self.tabBarController?.selectedIndex = 3
     }
     @IBAction func aboutUsBtnWasPressed(_ sender: Any) {
         print("AboutUS Pressed")
+        self.tabBarController?.selectedIndex = 1
     }
     @IBAction func contactUsBtnWasPressed(_ sender: Any) {
         print("ContactUs Pressed")
+        self.tabBarController?.selectedIndex = 4
     }
     @IBAction func socialLinksBtnWasPressed(_ sender: Any) {
         print("SocialLinks Pressed")
