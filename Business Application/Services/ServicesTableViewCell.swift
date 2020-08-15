0edec87a41a42bf92444580389a07834440e4961
Benjamin Inemugha <bennyblasco@gmail.com>
2020-08-15 11:33:08 +0100

    Service TableViewCell

---
+++ Business Application/Services/ServicesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ServicesTableViewCell.swift
+//  Business Application
+//
+//  Created by Benjamin Inemugha on 15/08/2020.
+//  Copyright © 2020 Techeloper. All rights reserved.
+//
+
+import UIKit
+
+class ServicesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
