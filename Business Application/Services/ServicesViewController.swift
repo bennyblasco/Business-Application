5b478b2e27225cdd4286d13b44c90f5fa82aee93
Benjamin Inemugha <bennyblasco@gmail.com>
2020-08-17 23:35:39 +0100

    Services ViewController

---
+++ Business Application/Services/ServicesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ServicesViewController.swift
+//  Business Application
+//
+//  Created by Benjamin Inemugha on 17/08/2020.
+//  Copyright © 2020 Techeloper. All rights reserved.
+//
+
+import UIKit
+
+class ServicesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
