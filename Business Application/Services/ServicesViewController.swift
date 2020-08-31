58a6e1707aa81560cb82e0fc7166530b5475be78
Benjamin Inemugha <bennyblasco@gmail.com>
2020-08-31 23:22:45 +0100

    Services

--- Business Application/Services/ServicesViewController.swift
+++ Business Application/Services/ServicesViewController.swift
@@ -13,12 +13,34 @@ class ServicesViewController: UIViewController {
     @IBOutlet weak var detailImage: UIImageView!
     @IBOutlet weak var detailDescription: UITextView!
     
-    var sentData: String!
+    var sentData:String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.title = sentData
+        
+        if navigationItem.title == "Service 1"{
+            detailImage.image = UIImage(named: "Image1")
+            detailDescription.text = "This is image 1"
+        }
+        if navigationItem.title == "Service 2"{
+            detailImage.image = UIImage(named: "Image2")
+            detailDescription.text = "This is image 2"
+        }
+        if navigationItem.title == "Service 3"{
+            detailImage.image = UIImage(named: "Image3")
+            detailDescription.text = "This is image 3"
+        }
+        if navigationItem.title == "Service 4"{
+            detailImage.image = UIImage(named: "Image4")
+            detailDescription.text = "This is image 4"
+        }
+        if navigationItem.title == "Service 5"{
+            detailImage.image = UIImage(named: "Image5")
+            detailDescription.text = "This is image 5"
+        }
+        
     }
     
 
