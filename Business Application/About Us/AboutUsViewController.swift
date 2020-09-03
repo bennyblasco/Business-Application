84eab96ec43b4b600f09d4c92935d4bc9eb66f4a
Benjamin Inemugha <bennyblasco@gmail.com>
2020-09-03 09:36:52 +0100

    About Us

---
+++ Business Application/About Us/AboutUsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutUsViewController.swift
+//  Business Application
+//
+//  Created by Benjamin Inemugha on 03/09/2020.
+//  Copyright © 2020 Techeloper. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    @IBOutlet weak var contactUsButton: UIButton!
+    @IBOutlet weak var logo: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contactUsButton.layer.cornerRadius = 10
+        logo.layer.cornerRadius = 20
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func contactUsButtonWasPressed(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 4
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
