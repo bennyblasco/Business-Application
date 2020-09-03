584b53e89bcdaeb8770c98ea1f275059818f6fa2
Benjamin Inemugha <bennyblasco@gmail.com>
2020-09-03 11:39:54 +0100

    Collection view for portfolio

---
+++ Business Application/Portfolio/PortfolioCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PortfolioCollectionViewCell.swift
+//  Business Application
+//
+//  Created by Benjamin Inemugha on 03/09/2020.
+//  Copyright © 2020 Techeloper. All rights reserved.
+//
+
+import UIKit
+
+class PortfolioCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+}
