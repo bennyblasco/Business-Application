7d69857cde291a1290e0a3f6f137049e6ded023d
Benjamin Inemugha <bennyblasco@gmail.com>
2020-09-27 22:19:53 +0100

    Social

---
+++ Social /SocialTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SocialTableViewCell.swift
+//  Business Application
+//
+//  Created by Benjamin Inemugha on 27/09/2020.
+//  Copyright © 2020 Techeloper. All rights reserved.
+//
+
+import UIKit
+
+class SocialTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
