515272a1760268aa38e3cc90841d3ff70c3f0b02
Benjamin Inemugha <bennyblasco@gmail.com>
2020-10-02 00:09:57 +0100

    Cell

--- Social /SocialTableViewController.swift
+++ Social /SocialTableViewController.swift
@@ -37,7 +37,7 @@ class SocialTableViewController: UITableViewController {
 
   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         // Configure the cell...
 
