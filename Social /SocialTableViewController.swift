7f5270630be58e77b655ebf88ff81cd577d1ed0d
Benjamin Inemugha <bennyblasco@gmail.com>
2020-10-07 22:40:25 +0100

    NavigationItem

--- Social /SocialTableViewController.swift
+++ Social /SocialTableViewController.swift
@@ -22,6 +22,7 @@ class SocialTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         tableView.backgroundView = UIImageView(image: UIImage(named: "Background"))
+        self.navigationItem.title = "Social Links"
     }
 
     // MARK: - Table view data source
